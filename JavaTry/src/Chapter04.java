763cc62612b167a3cb7b7b664e72baa63a993ab2
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-08 22:51:20 +0900

    [update]

---
+++ JavaTry/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "syo";
+		System.out.println(name);
+		
+		name = "アイウエオ";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 5;
+        System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ JavaTry/src/Variable02.java
@@ -0,0 +1,9 @@
+
+public class Variable02 {
+    public static void main(String[] args) {
+        // 定数の宣言
+        final int MIN_NUMBER = 30;
+        System.out.println(MIN_NUMBER);
+    }
+
+}
