3387fd63602611a1bd22aef852dd9c285c5c7d30
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-10 16:43:10 +0900

    [update]

---
+++ JavaTry/src/Array01.java
@@ -0,0 +1,21 @@
+
+public class Array01 {
+	public static void main(String[] args) {
+		int[] array1 = new int[3];
+		array1[0] = 60;
+		array1[1] = 20;
+		array1[2] = 20;
+		
+		int int2 = array1[2];
+		
+		int array2[] = null;
+		
+		int[] array3 = new int[] { 10, 20, 50 };
+		int[] array4 = { 10, 20, 50 };
+		
+		System.out.println(array1[0]);
+//		System.out.println(array2[0]);
+		System.out.println(array3[0]);
+		System.out.println(array4[0]);
+	}
+}
---
+++ JavaTry/src/Null01.java
@@ -0,0 +1,15 @@
+
+public class Null01 {
+	public static void main(String[] args) {
+		String string1;
+		string1 = "アイウ";
+		System.out.println(string1 + "の文字数" + string1.length());
+		
+		string1 = "";
+		System.out.println(string1 + "の文字数" + string1.length());
+		
+//		nullが代入されたstring1を参照するとコンパイルエラー
+		string1 = null;
+		System.out.println(string1 + "の文字数" + string1.length());
+	}
+}
