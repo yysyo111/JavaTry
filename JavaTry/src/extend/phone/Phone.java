6b32a330de41138555b50bfb7b08eaf82490151a
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-20 22:11:37 +0900

    [update]

---
+++ JavaTry/src/extend/phone/Phone.java
@@ -0,0 +1,12 @@
+package extend.phone;
+
+public interface Phone {
+    final int MAX_NUMBER_DIGITS = 11;
+
+    void call(String number);
+
+    // defaultメソッドの宣言
+    default void powerOff() {
+        System.out.println("電源を切ります。");
+    }
+}
