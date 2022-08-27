603011bf488d95cb59ae81677009487adfcaf5f8
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-27 22:31:52 +0900

    [update]

---
+++ JavaTry/src/practice/dentaku4.java
@@ -0,0 +1,103 @@
+package practice;
+
+import java.util.Scanner;
+
+public class dentaku4 {
+
+public static void main(String[] args) throws InterruptedException {
+
+Scanner sc = new Scanner(System.in);
+
+// 標準入力を1行ずつ読み出す。
+  out : while (sc.hasNextLine()) {
+  String line = sc.nextLine();
+
+String[] arr = line.split(" ");
+
+double total = 0;
+
+if (arr.length < 3 || ((arr.length != 3) && (arr.length % 3 != 2))) {
+
+System.out.println("数値 演算子 数値 演算子 数値 … の順に最低3つの引数を指定してください");
+  continue;
+
+} else {
+
+// 先頭の数値
+  if (checkNum(arr[0])) {
+
+//数値変換
+  total = Double.parseDouble(arr[0]);
+
+}
+
+for (int i = 0; i <= ((arr.length - 3) / 2); i++) {
+
+//先頭の数値以降は一つとびに演算子と数値が続く
+  if (checkOperator(arr[i * 2 + 1]) && checkNum(arr[i * 2 + 2])) {
+
+//演算子によってtotalに合算する処理を変更
+  switch (arr[i * 2 + 1]) {
+
+case "+":
+  total = total + Double.parseDouble(arr[i * 2 + 2]);
+  break;
+
+case "-":
+  total = total - Double.parseDouble(arr[i * 2 + 2]);
+  break;
+
+case "*":
+  total = total * Double.parseDouble(arr[i * 2 + 2]);
+  break;
+
+case "/":
+  total = total / Double.parseDouble(arr[i * 2 + 2]);
+  break;
+
+}
+
+} else {
+  System.out.println("数値 演算子 数値 演算子 数値 … の順に引数を指定してください");
+  continue out;
+  }
+
+}
+
+  System.out.println("合計:" + total);
+  }
+  }
+
+sc.close();
+  }
+
+// 数値チェック 数値の場合にtrue 数値以外の場合false
+  private static boolean checkNum(String str) {
+
+try {
+
+// double型へ変換し、エラーが出なければ数値とみなす
+  Double.parseDouble(str);
+
+} catch (NumberFormatException e) {
+
+return false;
+
+}
+
+return true;
+
+}
+
+// 演算子チェック
+  private static boolean checkOperator(String str) {
+
+if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
+  return true;
+  } else {
+  return false;
+  }
+
+}
+
+}
