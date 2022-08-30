29f4813e822d121587da4c1ba3c4d77f88c61ed0
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-30 19:05:03 +0900

    [update]

---
+++ JavaTry/src/practice/ThreadSample1A.java
@@ -0,0 +1,28 @@
+package practice;
+public class ThreadSample1A {
+  public static void main(String[] args) {
+    Thread th1 = new ThreadA("Thread1");
+    Thread th2 = new ThreadA("Thread2");
+    
+    th1.start();
+    th2.start();
+  }
+}
+class ThreadA extends Thread {
+  String name;
+  int count = 0;
+  ThreadA(String name) {
+    this.name = name;
+  }
+  public void run(){
+    for ( int i = 0; i < 5; i++ ) {
+      count++;
+      System.out.println(name+"-"+count);
+      try {
+        Thread.sleep(500);
+      } catch ( InterruptedException e ) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
