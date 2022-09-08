a5a3b633a4c0b9c4019efa3e6238945563433dd4
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-09-08 15:31:28 +0900

    [update]

--- JavaTry/src/Hero01.java
+++ JavaTry/src/Hero01.java
@@ -1,6 +1,24 @@
+import practice.Matango;
+
 public class Hero01 {
+	
+	public Hero01() {
+		System.out.println("Heroのコンストラクタが動作");
+	}
+	
 	String name = "ミナト";
 	int hp = 100;
+	
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃");
+		m.hp -= 5;
+		System.out.println("5ポイントのダメージを与えた");
+	}
+	public final void slip() {
+		this.hp -= 5;
+		System.out.println(this.name + "は転んだ");
+		System.out.println("5のダメージ");
+	}
 	public void run() {
 		System.out.println(this.name + "は逃げ出した");
 	}
--- JavaTry/src/SuperHero.java
+++ JavaTry/src/SuperHero.java
@@ -1,5 +1,18 @@
+import practice.Matango;
+
 public class SuperHero extends Hero01 {
 	boolean flying;
+	
+	public SuperHero() {
+		System.out.println("SuperHeroのコンストラクタが動作!");
+	}
+	
+	public void attack(Matango m) {
+		super.attack(m);
+		if (this.flying) {
+			super.attack(m);
+		}
+	}
 	
 	public void fly() {
 		this.flying = true;
