d4c585d172440c2b49b17c30aaa3c4978e4bb12e
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-09-10 23:23:11 +0900

    [update]

---
+++ JavaTry/src/item/PoisonMatango.java
@@ -0,0 +1,21 @@
+package item;
+
+import practice.Hero;
+
+public class PoisonMatango extends Matango {
+	int poisonCount = 5;
+	public PoisonMatango(char suffix) {
+		super(suffix);
+	}
+	
+	public void attack(Hero h) {
+		super.attack(h);
+		if (this.poisonCount > 0) {
+			System.out.println("さらに毒の胞子をばら撒いた");
+			int dmg = h.hp / 5;
+			h.hp -= dmg;
+			System.out.println(dmg + "ポイントのダメージ");
+			this.poisonCount--;
+		}
+	}
+}
