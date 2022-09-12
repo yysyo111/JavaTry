7bf182dce44a1146af3c64f017c925ab6d573631
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-09-13 00:11:13 +0900

    [update]

--- JavaTry/src/try01/Main.java
+++ JavaTry/src/try01/Main.java
@@ -10,5 +10,18 @@ public class Main {
 		h.slip();
 		h.sit(25);
 		h.run();
+		
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		Matango m2 = new Matango();
+		m2.hp = 50;
+		m2.suffix = 'B';
+		
+		h.slip();
+		m1.run();
+		m2.run();
+		h.run();
 	}
 }
--- JavaTry/src/try01/Matango.java
+++ JavaTry/src/try01/Matango.java
@@ -3,4 +3,8 @@ package try01;
 public class Matango {
 	int hp;
 	final int LEVEL = 10;
+	char suffix;
+	public void run() {
+		System.out.println("おばけきのこ" + this.suffix + "は逃げ出した");
+	}
 }
