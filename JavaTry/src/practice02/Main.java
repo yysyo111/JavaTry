dcb0990b26e5ba00330c9c9d62224de3254139ec
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-09-17 17:24:24 +0900

    [update]

--- JavaTry/src/practice02/Main.java
+++ JavaTry/src/practice02/Main.java
@@ -5,21 +5,23 @@ public class Main {
 		Sword s = new Sword();
 		s.name = "炎の剣";
 		s.damage = 10;
-		Hero h = new Hero();
-		h.name = "ミナト";
-		h.hp = 100;
+		Hero h = new Hero("ミナト");
 		h.sword = s;
+		System.out.println(h.hp);
 		System.out.println("現在の武器は" + h.sword.name);
 		h.attack();
 		
-		Hero h1;
-		h1 = new Hero();
-		h1.hp = 100;
-		Hero h2;
-		h2 = h1;
-		h2.hp = 200;
-		System.out.println(h1.hp);
 		
+		
+		Hero h1 = new Hero();
+		h1.hp = 100;
+		Hero h2 = new Hero("アサカ");
+		h2.hp = 100;
+		Wizard w = new Wizard();
+		w.hp = 50;
+		w.heal(h1);
+		w.heal(h2);
+		w.heal(h2);
 		
 		h.sit(5);
 		h.slip();
