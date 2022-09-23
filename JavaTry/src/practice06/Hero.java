dd3926bdbe23d9b0c3d8eccde7fef68188fbf65e
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-09-23 16:00:15 +0900

    [update]

--- JavaTry/src/practice06/Hero.java
+++ JavaTry/src/practice06/Hero.java
@@ -3,4 +3,7 @@ package practice06;
 public class Hero {
 	String name;
 	int hp;
+	public String toString() {
+		return "名前:" + this.name + "/HP:" + this.hp;
+	}
 }
--- JavaTry/src/practice06/Main.java
+++ JavaTry/src/practice06/Main.java
@@ -8,6 +8,24 @@ public class Main {
 		Object o1 = new Empty();
 		Object o2 = new Hero();
 		Object o3 =  "こんにちは";
+		
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println(h.toString());
+		System.out.println(h);
+		
+		Hero h1 = new Hero();
+		h1.name = "ミナト";
+		h1.hp = 100;
+		Hero h2 = new Hero();
+		h2.name = "ミナト";
+		h2.hp = 100;
+		if (h1.equals(h2) == true) {
+			System.out.println("同じ内容です");
+		} else {
+			System.out.println("違う内容です");
+		}
 	}
 	public void printAnything(Object o) {
 		System.out.println(o.toString());
