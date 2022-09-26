2119e6b37c7305cdbf9faced315df5d2da6745ba
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-09-26 22:00:54 +0900

    [update]

---
+++ JavaTry/src/practice09/Main02.java
@@ -0,0 +1,9 @@
+package practice09;
+
+public class Main02 {
+	public static void main(String[] args) {
+		String s1 = "Java programming";
+		System.out.println("文字列s1の４文字目以降は" + s1.substring(3));
+		System.out.println("文字列s1の４〜8文字目は" + s1.substring(3, 8));
+	}
+}
