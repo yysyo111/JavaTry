f2f2ea883e9a44e6322e7153ba8cfdf252aa66dd
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-10-01 22:43:46 +0900

    [update]

---
+++ JavaTry/src/practice10/Main06.java
@@ -0,0 +1,14 @@
+package practice10;
+
+public class Main06 {
+	 public static void main(String[] args) {
+ 	    int val = 11;
+	    if (val == 10) {
+	      System.out.println("変数valは10です");
+	    } else if (val == 20) {
+	      System.out.println("変数valは20です");
+	    } else {
+	      System.out.println("変数valは10でも20でもありません");
+	    }   // 結果：変数valは10でも20でもありません
+	 }
+}
