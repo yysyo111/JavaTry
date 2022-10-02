66af313446a7ace1529ad646b4d78b2263f4cac0
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-10-02 21:13:01 +0900

    [update]

---
+++ JavaTry/src/practice10/Main07.java
@@ -0,0 +1,8 @@
+package practice10;
+
+public class Main07 {
+	 public static void main(String[] args) {
+		    String str = "  あなた わたし かれ ";
+		    System.out.println(str.trim()); // 結果：「あなた わたし かれ」
+		  }
+}
