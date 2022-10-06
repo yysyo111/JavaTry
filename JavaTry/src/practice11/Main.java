968f299544fed01c5a467f4e1f2562a500b88693
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-10-06 16:32:42 +0900

    [update]復習

--- JavaTry/src/practice11/Main.java
+++ JavaTry/src/practice11/Main.java
@@ -1,5 +1,38 @@
 package practice11;
 
 public class Main {
-
+	//クラスブロック
+	public static void main(String[] args) {
+//		メソッドブロック
+		System.out.println("Hello World");
+		System.out.println("sugawara");
+		System.out.println("10 + 10の計算をします");
+		System.out.println(10 + 10);
+		int x;
+		x = 6;
+		System.out.println(x);
+		int value;
+		value = 23;
+		System.out.println(value);
+		int ver; //変数宣言
+		ver = 1 * 5;// 値代入
+		System.out.println("ver" + ver); //命令実行文
+		
+		final double PI = 3.14;
+		int pie = 5;
+		System.out.println("半径" + pie + "cmの牌の面積は、" + pie * pie * PI);
+		System.out.println(pie * pie * PI);
+	}
 }
+
+//ソースコード作成(人が読める状態のプログラミング、ソースファイル)→コンパイル(クラスファイル作成)→実行
+//{}ブロック
+//インデント
+//コメント
+/* */
+//
+
+//変数とはデータを格納するためにコンピュータ内部に準備する箱
+//型 変数名;
+//変数の初期化 変数宣言と代入を同時にすること
+//型 変数名 = 代入するデータ;
