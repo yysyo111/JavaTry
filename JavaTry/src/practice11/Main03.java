b634f2c22fa5d5c8b4b4c79fa1e26f198dca709c
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-10-06 18:14:19 +0900

    [update]

---
+++ JavaTry/src/practice11/Main03.java
@@ -0,0 +1,11 @@
+package practice11;
+
+public class Main03 {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int m = x + y;
+		String ans = "x+yは" + m;
+		System.out.println(ans);
+	}
+}
---
+++ JavaTry/src/practice11/Main04.java
@@ -0,0 +1,17 @@
+package practice11;
+
+public class Main04 {
+	public static void main(String[] args) {
+		System.out.println("ようこそ占いの館へ");
+		System.out.println("あなたの名前を入力してください");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢を入力してください");
+		String ageString = new java.util.Scanner(System.in).nextLine();
+		int age = Integer.parseInt(ageString);
+		int fortune = new java.util.Random().nextInt(3);
+		fortune++;
+		System.out.println("占いの結果が出ました！");
+		System.out.println(age + "さいの" + name + "さん" + "あなたの運気番号は" + fortune + "です");
+		System.out.println("1:大吉 2:中吉 3:吉 4:凶");
+	}
+}
