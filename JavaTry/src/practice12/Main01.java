e0c79143b3ca8f2ac47f8bc29a519766812f0670
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-10-16 23:03:57 +0900

    [update]set

---
+++ JavaTry/src/practice12/Main01.java
@@ -0,0 +1,28 @@
+package practice12;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main01 {
+	public static void main(String[] args) {
+		Set<String> colors = new HashSet<String>();
+		colors.add("赤");
+		colors.add("青");
+		colors.add("黄");
+		colors.add("赤");
+		System.out.println("色は" + colors.size() + "種類");
+		for (String s : colors) {
+			System.out.println(s + "→");
+		}
+		
+		Set<String> words = new TreeSet<String>();
+		words.add("dog");
+		words.add("cat");
+		words.add("wolf");
+		words.add("panda");
+		for (String s : words) {
+			System.out.print(s + "→");
+		}
+	}
+}
