9592014c3689dcf4eebde24e6dc7bdd2d2f04542
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-10-23 13:15:37 +0900

    [update]

---
+++ JavaTry/src/practice12/Main03.java
@@ -0,0 +1,18 @@
+package practice12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main03 {
+	public static void main(String[] args) {
+		Map<String, Integer> prefs = new HashMap<String, Integer>();
+		prefs.put("京都府", 255);
+		prefs.put("東京都", 1261);
+		prefs.put("熊本県", 181);
+		
+		for (String key : prefs.keySet()) {
+			int value = prefs.get(key);
+			System.out.println(key + "の人口は、" + value);
+		}
+	}
+}
