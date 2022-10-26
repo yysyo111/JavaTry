317d9bce3bb94a8556e5e071a6f550ce7ec442b5
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-10-26 23:14:35 +0900

    [update]

--- JavaTry/src/practice12/Main04.java
+++ JavaTry/src/practice12/Main04.java
@@ -7,13 +7,16 @@ public class Main04 {
 	public static void main(String[] args) {
 		Hero h = new Hero();
 		h.name = "ミナト";
+		h.hp = 100;
 		List<Hero> list = new ArrayList<Hero>();
 		list.add(h);
 		h.name = "スガワラ";
 		System.out.println(list.get(0).name);
+		System.out.println(list.get(0).hp);
 	}
 }
 
 class Hero {
 	public String name;
+	public int hp;
 }
